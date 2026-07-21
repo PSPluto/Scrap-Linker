b1a9212fc42da6390441fa04736014035c562e02
Pluto <aokitaisei217@gmail.com>
2026-07-21 15:56:12 +0900

    Gamemanagerを追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public static class GameManager
+{
+    public enum GameState
+    {
+        Title,
+        InGame,
+        GameOver,
+    };
+
+    public static GameState gameState =  GameState.Title;
+
+    public static GameStateChangeLog UpdateGameState(GameState nextState)
+    {
+        GameStateChangeLog returnState = new GameStateChangeLog { beforeState = gameState, afterState = nextState};
+        gameState = nextState;
+        return returnState;
+    }
+    
+}
+
+public struct GameStateChangeLog
+{
+    public GameManager.GameState beforeState;
+    public GameManager.GameState afterState;
+}
