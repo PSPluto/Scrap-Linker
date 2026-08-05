e253bb4fc08a3c674b5aaa292f8b970592452f1d
akiton20100217-pixel <akiton20100217@gmail.com>
2026-08-05 16:43:08 +0900

    HeavyMetalのスクリプトを追加

--- Assets/Scripts/Enemy/HeavyMatal.cs
+++ Assets/Scripts/Enemy/HeavyMatal.cs
@@ -2,24 +2,5 @@ using UnityEngine;
 
 public class HeavyMatal : BaseEnemy
 {
-    protected override void Update()
-    {
-        base.Update();
-    }
-
-    public override void Attack()
-    {
-        base.Attack();
-        Debug.Log ("HeavyMatal Attack");
-    }
     
-    public override void EndAttack()
-    {
-        base.EndAttack();
-    }
-
-    public override void TakeDamage(float damageAmount)
-    {
-        base.TakeDamage(damageAmount);
-    }
 }
